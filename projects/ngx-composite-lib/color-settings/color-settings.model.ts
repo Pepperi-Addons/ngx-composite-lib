dbfa0d468be95c9d02ac62a2d2dc20ceec35d3d3
Avner Eisenberg <avner.e@pepperi.com>
2022-08-10 15:14:07 +0300

    add contractors and defaults to colors and shadow

--- projects/ngx-composite-lib/color-settings/color-settings.model.ts
+++ projects/ngx-composite-lib/color-settings/color-settings.model.ts
@@ -1,6 +1,12 @@
 export class PepColorSettings {
-    use = false;
-    value = 'hsl(0, 0%, 57%)';
+    use: boolean;
+    value: string;
     opacity = 50;
     // useGradientOverlay?: boolean = true;
+
+    constructor(use: boolean = false, value: string = 'hsl(0, 0%, 57%)', opacity = 50) {
+        this.use =  use;
+        this.value = value;
+        this.opacity = opacity;
+      }
 }
