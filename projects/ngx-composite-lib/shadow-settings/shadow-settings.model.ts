43b2f7a43f385cd7abe4a990cc3706ea81fe315c
Avner Eisenberg <avner.e@pepperi.com>
2022-08-10 12:26:19 +0300

    change the default PepShadowIntensityType from regular to soft DI-20970

--- projects/ngx-composite-lib/shadow-settings/shadow-settings.model.ts
+++ projects/ngx-composite-lib/shadow-settings/shadow-settings.model.ts
@@ -5,5 +5,5 @@ export type PepShadowIntensityType = 'soft' | 'regular' | 'hard';
 export class PepShadowSettings {
     use = false;
     size: PepSizeType = 'md';
-    intensity: PepShadowIntensityType = 'regular';
+    intensity: PepShadowIntensityType = 'soft';
 }
